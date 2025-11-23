b328da4fed3e84e8790f19f4f607250640b1cb15
IdontIanKnow <ianramitezpadilla@gmail.com>
2025-11-22 20:02:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests, os
 
 app = Flask(__name__)
+CORS(app, origins=["https://idontianknow.github.io"])
 
 API_KEY = os.getenv("SPOONACULAR_KEY")
 
